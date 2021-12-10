bb9bcd765cdf32e5b50200d8e5f611cd418aff2e
Diego <diegorf55@gmail.com>
2021-12-10 00:12:08 -0300

    life cycle components

--- src/App.js
+++ src/App.js
@@ -5,51 +5,34 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nome: "Diego",
-      contador: 0
+      hora: '00:00:00'
     };
+  }
 
-     this.aumentar = this.aumentar.bind(this)
-     this.diminuir = this.diminuir.bind(this)
+  //só é executado uma única vez, após a montagem
+  componentDidMount() {
+
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() } )
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    console.log("Atualizou")
+  }
+
+  //retorna true ou false
+  shouldComponentUpdate(){
 
   }
   
-aumentar() {
-
-  let state = this.state;
-  state.contador += 1;
-  state.nome = 'Maria';
-
-  this.setState(state)
-  
-}
-
-diminuir() {
-  let state = this.state;
-  
-  if(state.contador === 0){
-    alert("O valor não pode ser menor que ZERO!")
-    return
-  }
-  
-  state.contador -= 1;
-  this.setState(state)
-}
-
-
-
   render() {
     return (
       <div>
-        <h1>Contador</h1>
-        {this.state.nome}
-          <h3>
-            <button onClick={this.diminuir}> - </button>
-              {this.state.contador}
-            <button onClick={this.aumentar} > + </button>
-          </h3>
+        <h1>Meu projeto {this.state.hora}</h1>
+       
       </div>
-    )
+    );
   }
 }
 
