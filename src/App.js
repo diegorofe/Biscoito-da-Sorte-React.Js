15c5c7996e0ef6cd224ac6475796a267f3969117
Diego <diegorf55@gmail.com>
2021-12-11 11:41:39 -0300

    manipulando formulários

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import Feed from './components/feed';
+
 
 export default class App extends Component {
 
 constructor(props){
   super(props);
   this.state = {
-    feed: [
-      {id: 1, username: 'Matheus', curtidas: 10, comentarios: 2},
-      {id: 2, username: 'Diego', curtidas: 15, comentarios: 3},
-      {id: 3, username: 'Amanda', curtidas: 8, comentarios: 12},
-      {id: 4, username: 'Thais', curtidas: 1, comentarios: 0}
-    ]
+    email: "usuario@email.com",
+    senha: "12345"
+   
   };
+
+  this.trocaEmail = this.trocaEmail.bind(this)
+  this.trocaSenha = this.trocaSenha.bind(this)
+}
+
+trocaEmail(e){
+
+  let valorDigitado = e.target.value;
+  this.setState({email: valorDigitado});
+}
+
+trocaSenha(e){
+
+  let valorDigitado = e.target.value;
+  this.setState({senha: valorDigitado})
 }
 
 
@@ -20,16 +32,28 @@ constructor(props){
 render(){
   return(
     <div>
-      {this.state.feed.map( (item) => {
-        return(
-          <Feed 
-          id={item.id} 
-          username={item.username} 
-          curtidas={item.curtidas}
-          comentarios={item.comentarios} />
-        );
-      })}
+     <h3>Sistema de Login</h3>
+     Email:
+     <input onChange={this.trocaEmail}  type="email" value= {this.state.email} name="email"></input> <br/>
+     Senha:
+     <input onChange={this.trocaSenha}  type="password" value= {this.state.senha} name="senha"></input> <br/>
+     Sexo:
+     <select name="sexo" value={this.state.sexo} onChange={(e) =>
+    this.setState({sexo: e.target.value})}>
+       <option value="Masculino">Masculino</option>
+       <option value="Feminino">Feminino</option>
+     </select>
+      <br/>
+      <br/>
+ 
+     <div>
+    Verificação das states
+       <h3>{this.state.email}</h3>
+       <h3>{this.state.senha}</h3>
+       <h3>{this.state.sexo}</h3>
+     </div>
     </div>
+
   );
 }
 
